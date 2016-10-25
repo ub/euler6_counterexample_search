9e02de4488f1892e0808a9ba272ba5613ce09572
Yuri Baranov <baranovu@gmail.com>
2016-10-25 08:42:43 +0300

    testing  TwoTermsAllButOneTermDivisibleBy_p_Tactic

--- spec/factories.rb
+++ spec/factories.rb
@@ -5,6 +5,10 @@ FactoryGirl.define do
     factory :hyp3 do
       terms_count 3
     end
+    factory :hyp2 do
+      terms_count 2
+    end
+
 
   end
 
--- spec/goal_replacement_spec.rb
+++ spec/goal_replacement_spec.rb
@@ -47,3 +47,108 @@ describe GoalReplacement::Modulo_19_Tactic do
   end
 
 end
+
+
+
+
+describe GoalReplacement::TwoTermsAllButOneTermDivisibleBy_p_Tactic do
+  context 'modulo 5' do
+    let (:match1) {FactoryGirl.build(:hyp2, value: 5**6 + 1)}
+    let (:match_1) {FactoryGirl.build(:hyp2, value: 5**6 + 3 **6)}
+    let (:nomatch_0) {FactoryGirl.build(:hyp2, value: 5**6 )}
+    let (:nomatch_2) {FactoryGirl.build(:hyp2, value: 1 + 6**6 )}
+    let (:nomatch_3) {FactoryGirl.build(:hyp2, value: 2**6 + 3**6 )}
+
+    subject {GoalReplacement::TwoTermsAllButOneTermDivisibleBy_p_Tactic.new 5}
+
+    describe '#match?' do
+       it 'matches modulo +-1' do
+         expect(subject.match?(match1)).to be_truthy
+         expect(subject.match?(match_1)).to be_truthy
+       end
+
+       it 'does not match non-residues' do
+         expect(subject.match?(nomatch_0)).to be_falsey
+         expect(subject.match?(nomatch_2)).to be_falsey
+         expect(subject.match?(nomatch_3)).to be_falsey
+       end
+    end
+
+    describe '#apply' do
+      it 'generates the only possible hypothesis for 1 residue example' do
+        expect{|b|
+          subject.apply(match1, &b)
+        }.to yield_control.once
+
+        expect{|b|
+          subject.apply(match1, &b)
+        }.to yield_with_args(an_object_having_attributes(value:1, factor: 5**6, terms_count:1))
+
+      end
+
+      it 'generates the only possible hypothesis for -1 residue example' do
+        expect{|b|
+          subject.apply(match_1, &b)
+        }.to yield_control.once
+
+        expect{|b|
+          subject.apply(match1, &b)
+        }.to yield_with_args(an_object_having_attributes(value:1, factor: 5**6, terms_count:1))
+
+      end
+
+    end
+
+
+  end
+  context 'modulo 43' do
+    #[1, 4, 11, 16, 21, 35, 41]
+    let (:match1) {FactoryGirl.build(:hyp2, value: 6**6 + 5*43**6 )}
+     let (:match41) {FactoryGirl.build(:hyp2, value:   3 **6 + 43**6)}
+    let (:nomatch_0) {FactoryGirl.build(:hyp2, value: 4*43**6 )}
+     let (:nomatch_2) {FactoryGirl.build(:hyp2, value: 7**6 + 6**6 )}
+
+    subject {GoalReplacement::TwoTermsAllButOneTermDivisibleBy_p_Tactic.new 43}
+
+    describe '#match?' do
+       it 'matches modulo 1' do
+         expect(subject.match?(match1)).to be_truthy
+         expect(subject.match?(match41)).to be_truthy
+       end
+
+       it 'does not match non-residues' do
+         expect(subject.match?(nomatch_0)).to be_falsey
+         expect(subject.match?(nomatch_2)).to be_falsey
+       end
+    end
+
+    describe '#apply' do
+      it 'generates the only possible hypothesis for 1 residue example' do
+        expect{|b|
+          subject.apply(match1, &b)
+        }.to yield_control.once
+
+        expect{|b|
+          subject.apply(match1, &b)
+        }.to yield_with_args(an_object_having_attributes(value:5, factor: 43**6, terms_count:1))
+
+      end
+
+      it 'generates the only possible hypothesis for 41 residue example' do
+        expect{|b|
+          subject.apply(match41, &b)
+        }.to yield_control.once
+
+        expect{|b|
+          subject.apply(match41, &b)
+        }.to yield_with_args(an_object_having_attributes(value:1, factor: 43**6, terms_count:1))
+
+      end
+
+    end
+
+
+  end
+
+
+end
