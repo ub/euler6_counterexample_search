080d00c2a88d224afa48be7e6e5d5bf3123e2cdf
Yuri Baranov <baranovu@gmail.com>
2016-12-01 12:10:29 +0300

    spec fixed -- refutations are not persisted on creation now but imported in batches via active_record_migrations

--- spec/euler_sop6_conjecture_counterexample_search_spec.rb
+++ spec/euler_sop6_conjecture_counterexample_search_spec.rb
@@ -6,10 +6,10 @@ describe EulerSop6ConjectureCounterexampleSearch::Filter4 do
   it 'refutes value which is divisible by 8 but not 64' do
     expect(subject.filter([regression2a_bad])).to be_empty
     expect(regression2a_bad.refutation).to be_present
-    expect(regression2a_bad.refutation.reload.hypothesis).to eq(regression2a_bad)
+    expect(regression2a_bad.refutation.hypothesis).to eq(regression2a_bad)
   end
 
-fdescribe EulerSop6ConjectureCounterexampleSearch::Process3 do
+describe EulerSop6ConjectureCounterexampleSearch::Process3 do
   let (:parent_goal) {FactoryGirl.create(:hyp3, value: 8897951778164227,factor:729)}
   specify do
     # just for debugging - nothing interesting
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,10 +14,7 @@ require 'hypothesis'
 
 
 require 'modulo_k_6th_root_generator'
-require 'euler6_counterexample_search'
 require 'sums_of6th_power_m_terms_mod_k'
-require 's6p_hypothesis'
-require 'sum_of2_cubic_squares_fast_checker'
 
 
 
