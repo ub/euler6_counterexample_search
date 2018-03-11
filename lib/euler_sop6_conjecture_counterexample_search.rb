d46b18f37161840c9aac26e32d95ba59cef7be46
Yuri Baranov <baranovu@gmail.com>
2018-03-12 00:23:41 +0300

    bin/generate5 accepts parameters and prints more stats

--- lib/euler_sop6_conjecture_counterexample_search.rb
+++ lib/euler_sop6_conjecture_counterexample_search.rb
@@ -8,14 +8,17 @@ require 'goal_replacement'
 
 module EulerSop6ConjectureCounterexampleSearch
   class StartHypothesesGenerator
-    def generate
-      (1...117649).each.lazy.select do |x|
+    def generate(start=1, stop=117649)
+      count = 0
+      (start...stop).each.lazy.select do |x|
         x % 2 != 0 &&
             x % 3 != 0 &&
             x % 7 != 0
       end.each do |x|
+        count +=1
         Hypothesis.create!(value: x**6, terms_count: 5, factor: 1)
       end
+      count
     end
   end
 
